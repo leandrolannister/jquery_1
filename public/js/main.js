0acfac4800d9270005675ec297fcbda715ed5bca
leandrolannister <leandrohendrix@gmail.com>
2021-04-17 16:40:27 -0300

    AL2->Eventos

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,14 @@ $('#phraseSize').text( () => {
   return $('.phrase').text().split(" ").length;    
 });
 
+$('.textarea').on("input",() => {
+  
+  $('#count_letters').text( () => {
+    return $('.textarea').val().length;
+  });
+
+  $('#count_words').text( () => {
+    return $('.textarea').val().split(/\S+/).length;
+  });
+});
+
